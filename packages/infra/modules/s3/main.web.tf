056d0b60b8b817031451b64d1104183fc7e31cb4
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-19 17:09:26 +0100

    add cloudfront

--- packages/infra/modules/s3/main.web.tf
+++ packages/infra/modules/s3/main.web.tf
@@ -2,6 +2,11 @@ resource "aws_s3_bucket" "kairos_web_bucket" {
   bucket = format("kairos-web-%s", var.random_name)
 }
 
+resource "aws_s3_bucket_acl" "kairos_web_bucket_acl" {
+  bucket = aws_s3_bucket.kairos_web_bucket.id
+  acl    = "private"
+}
+
 resource "aws_s3_bucket_website_configuration" "kairos_web_website_configuration" {
   bucket = aws_s3_bucket.kairos_web_bucket.id
 
@@ -10,13 +15,24 @@ resource "aws_s3_bucket_website_configuration" "kairos_web_website_configuration
   }
 
   error_document {
-    key = "error.html"
+    key = "index.html"
   }
 }
 
-resource "aws_s3_bucket_versioning" "kairos_web_bucket_versioning" {
+resource "aws_s3_bucket_policy" "kairos_web_bucket_policy" {
   bucket = aws_s3_bucket.kairos_web_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "AllowGetObjects"
+    Statement = [
+      {
+        Sid       = "AllowPublic"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.kairos_web_bucket.arn}/**"
+      }
+    ]
+  })
 }
