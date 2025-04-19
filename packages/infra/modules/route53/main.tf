e25f04ac9f9ab6e213fd4fe050ee3d4e14040a8b
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-19 20:34:03 +0100

    fix

--- packages/infra/modules/route53/main.tf
+++ packages/infra/modules/route53/main.tf
@@ -23,7 +23,7 @@ resource "aws_acm_certificate_validation" "validation" {
 
 resource "aws_route53_record" "validation_record" {
   for_each = {
-    for dvo in var.domain_validation_options : dvo.domain_name => {
+    for idx, dvo in var.domain_validation_options : idx => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
