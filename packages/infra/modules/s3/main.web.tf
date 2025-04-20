a83a5f01bd1dfff7a87991bda292e2bd79765d1c
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-20 13:37:33 +0100

    do not block publick acls

--- packages/infra/modules/s3/main.web.tf
+++ packages/infra/modules/s3/main.web.tf
@@ -23,8 +23,8 @@ resource "aws_s3_bucket_policy" "kairos_web_bucket_policy" {
 resource "aws_s3_bucket_public_access_block" "kairos_web_bucket" {
   bucket = aws_s3_bucket.kairos_web_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
