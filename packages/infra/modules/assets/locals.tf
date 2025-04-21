7e0e04f44f968b665b2e1580c465b4c0c70ba630
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-21 12:51:38 +0100

    fix paths

--- packages/infra/modules/assets/locals.tf
+++ packages/infra/modules/assets/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  icons     = fileset("${path.module}/${locals.iconsPath}", "*.png")
+  icons     = fileset("${path.module}/icons", "*.png")
   iconsPath = "icons"
   iconsTargetPath = "assets/icons"
 }
