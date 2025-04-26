88696b915101270261dc5f5ed3682aef1c016616
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-26 13:38:11 +0100

    set timezone to london for jest

--- packages/web/jest.config.ts
+++ packages/web/jest.config.ts
@@ -10,6 +10,9 @@ const config: Config = {
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'identity-obj-proxy',
   },
+  globals: {
+    TZ: 'Europe/London',
+  },
 }
 
 export default config
