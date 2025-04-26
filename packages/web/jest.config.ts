a8168e2c01212bf56ae70032637ed48d08ced265
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-26 13:43:59 +0100

    set timezone using the command

--- packages/web/jest.config.ts
+++ packages/web/jest.config.ts
@@ -9,10 +9,7 @@ const config: Config = {
   preset: 'ts-jest',
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'identity-obj-proxy',
-  },
-  globals: {
-    TZ: 'Europe/London',
-  },
+  }
 }
 
 export default config
