ce7e7d337a0672c40682f1ef4d3e5382c0decc64
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-27 21:40:06 +0100

    improve permissions

--- packages/infra/main.tf
+++ packages/infra/main.tf
@@ -35,6 +35,7 @@ module "policies" {
   dynamodb_grocery_list_arn                = module.dynamodb.grocery_list_arn
   dynamodb_grocery_items_defaults_arn      = module.dynamodb.grocery_items_defaults_arn
   dynamodb_noise_tracking_arn              = module.dynamodb.noise_tracking_arn
+  dynamodb_todo_list_arn                   = module.dynamodb.todo_list_arn
   s3_kairos_web_arn                        = module.s3.kairos_web_arn
   s3_kairos_lambdas_arn                    = module.s3.kairos_lambdas_arn
 }
--- packages/infra/modules/policies/data.lambda.tf
+++ packages/infra/modules/policies/data.lambda.tf
@@ -15,21 +15,41 @@ data "aws_iam_policy_document" "lambda_policies" {
   }
 
   dynamic "statement" {
-    for_each = each.value.permissions.database == "read-only" ? [each.key] : []
+    for_each = each.value.permissions.database.grocery_list == local.permissions.read_only ? [each.key] : []
 
     content {
       sid = "DatabaseReadOnly"
-      actions = [
-        "dynamodb:GetItem",
-        "dynamodb:Scan",
-        "dynamodb:Query",
-      ]
+      actions = local.database_read_only_actions
       effect = "Allow"
       resources = [
         var.dynamodb_grocery_list_arn,
-        var.dynamodb_grocery_items_defaults_arn,
         "${var.dynamodb_grocery_list_arn}/index/*",
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = each.value.permissions.database.grocery_items_defaults == local.permissions.read_only ? [each.key] : []
+
+    content {
+      sid = "DatabaseReadOnly"
+      actions = local.database_read_only_actions
+      effect = "Allow"
+      resources = [
+        var.dynamodb_grocery_items_defaults_arn,
         "${var.dynamodb_grocery_items_defaults_arn}/index/*",
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = each.value.permissions.database.noise_tracking == local.permissions.read_only ? [each.key] : []
+
+    content {
+      sid = "DatabaseReadOnly"
+      actions = local.database_read_only_actions
+      effect = "Allow"
+      resources = [
         var.dynamodb_noise_tracking_arn,
         "${var.dynamodb_noise_tracking_arn}/index/*",
       ]
@@ -37,27 +57,71 @@ data "aws_iam_policy_document" "lambda_policies" {
   }
 
   dynamic "statement" {
-    for_each = each.value.permissions.database == "read-write" ? [each.key] : []
+    for_each = each.value.permissions.database.todo_list == local.permissions.read_only ? [each.key] : []
+
+    content {
+      sid = "DatabaseReadOnly"
+      actions = local.database_read_only_actions
+      effect = "Allow"
+      resources = [
+        var.dynamodb_todo_list_arn,
+        "${var.dynamodb_todo_list_arn}/index/*",
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = each.value.permissions.database.grocery_list == local.permissions.read_write ? [each.key] : []
 
     content {
       sid = "DatabaseReadWrite"
-      actions = [
-        "dynamodb:DeleteItem",
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:Scan",
-        "dynamodb:UpdateItem",
-        "dynamodb:BatchWriteItem",
-        "dynamodb:Query",
-      ]
+      actions = local.database_read_write_actions
       effect = "Allow"
       resources = [
         var.dynamodb_grocery_list_arn,
-        var.dynamodb_grocery_items_defaults_arn,
-        var.dynamodb_noise_tracking_arn,
         "${var.dynamodb_grocery_list_arn}/index/*",
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = each.value.permissions.database.grocery_items_defaults == local.permissions.read_write ? [each.key] : []
+
+    content {
+      sid = "DatabaseReadWrite"
+      actions = local.database_read_write_actions
+      effect = "Allow"
+      resources = [
+        var.dynamodb_grocery_items_defaults_arn,
         "${var.dynamodb_grocery_items_defaults_arn}/index/*",
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = each.value.permissions.database.noise_tracking == local.permissions.read_write ? [each.key] : []
+
+    content {
+      sid = "DatabaseReadWrite"
+      actions = local.database_read_write_actions
+      effect = "Allow"
+      resources = [
+        var.dynamodb_noise_tracking_arn,
         "${var.dynamodb_noise_tracking_arn}/index/*",
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = each.value.permissions.database.todo_list == local.permissions.read_write ? [each.key] : []
+
+    content {
+      sid = "DatabaseReadWrite"
+      actions = local.database_read_write_actions
+      effect = "Allow"
+      resources = [
+        var.dynamodb_todo_list_arn,
+        "${var.dynamodb_todo_list_arn}/index/*",
       ]
     }
   }
