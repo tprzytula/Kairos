abc2456ee79d62d8b964d0830f4cefc665c5ebf6
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-28 21:08:57 +0100

    simplify swagger

---
+++ packages/infra/modules/api_gateway/locals.tf
@@ -0,0 +1,26 @@
+locals {
+  openapi_files = {
+    for file in fileset("${path.module}/openapi", "*.yml") :
+    trimsuffix(file, ".yml") => trimsuffix(file, ".yml")
+  }
+
+  openapi_specs = {
+    for spec in local.openapi_files :
+    spec => yamldecode(
+      templatefile(
+        "${path.module}/openapi/${spec}.yml",
+        { lambda_functions = var.lambda_functions }
+      )
+    )
+  }
+
+  merged_paths = merge([
+    for spec in local.openapi_files :
+    local.openapi_specs[spec].paths
+  ]...)
+  
+  merged_components = merge([
+    for spec in local.openapi_files :
+    local.openapi_specs[spec].components
+  ]...)
+}
