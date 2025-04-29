c86baad20cc661cc99271ca955b22a327340b18c
tomasz.przytula <tomasz.przytula@ballys.com>
2025-04-29 21:31:53 +0100

    todo list provider

---
+++ packages/web/src/providers/ToDoListProvider/types.ts
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import { ITodoItem } from '../../api/toDoList/retrieve/types'
+
+export interface IGroceryListProviderProps {
+    children: ReactNode
+}
+
+export interface IState {
+    toDoList: Array<ITodoItem>
+    isLoading: boolean
+    refetchToDoList: () => Promise<void>
+}
