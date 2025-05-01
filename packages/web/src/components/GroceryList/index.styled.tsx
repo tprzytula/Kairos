f50376951f58c6dcb79bfdae84ffff6d46bbeff8
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:54 +0100

    show no items in your grocery list message

--- packages/web/src/components/GroceryList/index.styled.tsx
+++ packages/web/src/components/GroceryList/index.styled.tsx
@@ -1,8 +1,21 @@
 import { styled } from '@mui/material/styles'
-
+import { Typography } from '@mui/material'
 export const Container = styled('div')({
   display: 'flex',
   flexDirection: 'column',
   gap: '0.35em',
   width: '100%',
+})
+
+export const EmptyListContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.35em',
+  width: '100%',
+})
+
+export const EmptyListMessage = styled(Typography)({
+  fontSize: '1.2em',
+  fontWeight: 'bold',
+  textAlign: 'center',
 })
