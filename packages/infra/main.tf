253f34b47e09678ba526a0ca24a884f40e97473a
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:54 +0100

    try

--- packages/infra/main.tf
+++ packages/infra/main.tf
@@ -30,14 +30,14 @@ module "s3" {
 module "policies" {
   source = "./modules/policies"
 
-  random_name                              = module.random.random_name
-  lambda_functions                         = module.lambda.lambda_functions
-  dynamodb_grocery_list_arn                = module.dynamodb.grocery_list_arn
-  dynamodb_grocery_items_defaults_arn      = module.dynamodb.grocery_items_defaults_arn
-  dynamodb_noise_tracking_arn              = module.dynamodb.noise_tracking_arn
-  dynamodb_todo_list_arn                   = module.dynamodb.todo_list_arn
-  s3_kairos_web_arn                        = module.s3.kairos_web_arn
-  s3_kairos_lambdas_arn                    = module.s3.kairos_lambdas_arn
+  random_name                         = module.random.random_name
+  lambda_functions                    = module.lambda.lambda_functions
+  dynamodb_grocery_list_arn           = module.dynamodb.grocery_list_arn
+  dynamodb_grocery_items_defaults_arn = module.dynamodb.grocery_items_defaults_arn
+  dynamodb_noise_tracking_arn         = module.dynamodb.noise_tracking_arn
+  dynamodb_todo_list_arn              = module.dynamodb.todo_list_arn
+  s3_kairos_web_arn                   = module.s3.kairos_web_arn
+  s3_kairos_lambdas_arn               = module.s3.kairos_lambdas_arn
 }
 
 module "assets" {
