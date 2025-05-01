058aa765069e838b7ab75bc44b4c8e262a9f38bd
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:48 +0100

    provision new dynamodb for icons

---
+++ packages/infra/modules/dynamodb/main.grocery_items_icons.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "grocery_items_icons" {
+  name           = "GroceryItemsIcons"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "name"
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  attribute {
+    name = "iconPath"
+    type = "S"
+  }
+}
