aa696a19cef4cf6f52861f9b419f0d4c1a5ccacd
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:50 +0100

    grocery items icon table

--- packages/infra/main.tf
+++ packages/infra/main.tf
@@ -33,7 +33,7 @@ module "policies" {
   random_name                      = module.random.random_name
   lambda_functions                 = module.lambda.lambda_functions
   dynamodb_grocery_list_arn        = module.dynamodb.grocery_list_arn
-  # dynamodb_grocery_items_icons_arn = module.dynamodb.grocery_items_icons_arn
+  dynamodb_grocery_items_icons_arn = module.dynamodb.grocery_items_icons_arn
   s3_kairos_web_arn                = module.s3.kairos_web_arn
   s3_kairos_lambdas_arn            = module.s3.kairos_lambdas_arn
 }
