44cdcecba9665b0582e40e76078ecdbf982ec510
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:47 +0100

    mark as purchased, just frontend for now

--- packages/web/src/components/GroceryItem/index.styled.tsx
+++ packages/web/src/components/GroceryItem/index.styled.tsx
@@ -1,24 +1,38 @@
 import { styled } from "@mui/system";
 import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material'
 
-export const Container = styled(Card)({
+export const Container = styled(Card)(({ isPurchased }: { isPurchased: boolean }) => ({
     display: 'flex',
     borderRadius: 0.2,
     minHeight: '100px',
     width: '100%',
     justifyContent: 'flex-start',
     boxSizing: 'border-box',
-})
+    opacity: isPurchased ? 0.5 : 1,
+    transition: 'all 0.3s ease',
+    position: 'relative',
+    '&::after': isPurchased ? {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: '90%',
+      height: '2px',
+      backgroundColor: 'black',
+    } : {},
+}))
 
 export const ActionArea = styled(CardActionArea)({
   display: 'flex',
   padding: '0 2em',
 })
 
-export const Media = styled(CardMedia)({
+export const Media = styled(CardMedia)(({
   width: '100px',
   height: '100px',
-})
+  position: 'relative',
+}))
 
 export const Content = styled(CardContent)({
   display: 'flex',
