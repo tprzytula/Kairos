5b22d05b17ebb1a897255344f2b95195b99684b5
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:47 +0100

    try

--- packages/infra/modules/s3/main.web.tf
+++ packages/infra/modules/s3/main.web.tf
@@ -15,28 +15,6 @@ resource "aws_s3_bucket_website_configuration" "kairos_web_website_configuration
   }
 }
 
-data "aws_iam_policy_document" "kairos_web_bucket_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.kairos_web_bucket.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.kairos_web_distribution.iam_arn]
-    }
-  }
-
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.kairos_web_bucket.arn]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.kairos_web_distribution.iam_arn]
-    }
-  }
-}
-
 resource "aws_s3_bucket_policy" "kairos_web_bucket_policy" {
   bucket = aws_s3_bucket.kairos_web_bucket.id
   policy = data.aws_iam_policy_document.kairos_web_bucket_policy.json
