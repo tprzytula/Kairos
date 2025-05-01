99c929d21a33c87eaabe8d0f3c55fbd3a8ddfd22
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:46 +0100

    export bucket name

--- packages/infra/modules/s3/outputs.tf
+++ packages/infra/modules/s3/outputs.tf
@@ -2,6 +2,10 @@ output "kairos_web_arn" {
   value = aws_s3_bucket.kairos_web_bucket.arn
 }
 
+output "kairos_web_bucket_name" {
+  value = aws_s3_bucket.kairos_web_bucket.bucket
+}
+
 output "kairos_lambdas_arn" {
   value = aws_s3_bucket.kairos_lambdas_bucket.arn
 }
