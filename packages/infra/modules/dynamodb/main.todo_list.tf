b4c0aba6cd546334406365b99af35436f95db3d2
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:54 +0100

    create new database for todo list

---
+++ packages/infra/modules/dynamodb/main.todo_list.tf
@@ -0,0 +1,12 @@
+resource "aws_dynamodb_table" "todo_list" {
+  name           = "TodoList"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
