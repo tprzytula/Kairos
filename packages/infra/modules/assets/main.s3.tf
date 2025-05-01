9f4d4a576d296c135084bdc4d9506c605526d5fd
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:49 +0100

    fix paths

--- packages/infra/modules/assets/main.s3.tf
+++ packages/infra/modules/assets/main.s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_object" "icons" {
   for_each = { for icon in local.icons : icon => icon }
 
   bucket       = var.bucket_id
-  key          = "${local.iconsPath}/${each.value}"
+  key          = "${local.iconsTargetPath}/${each.value}"
   source       = "${path.module}/${local.iconsPath}/${each.value}"
   acl          = "public-read"
   content_type = "image/png"
