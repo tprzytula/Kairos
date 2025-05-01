4175e470406e9615d6bb57a5b2b0dfcec4af171a
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:51 +0100

    support reserved keys for dynamodb

--- packages/lambdas/libs/dynamodb/commands/query/utils.test.ts
+++ packages/lambdas/libs/dynamodb/commands/query/utils.test.ts
@@ -1,9 +1,9 @@
-import { getExpressionAttributeValues, getKeyConditionExpression } from "./utils";
+import { getExpressionAttributeNames, getExpressionAttributeValues, getKeyConditionExpression } from "./utils";
 
 describe("Given the getKeyConditionExpression function", () => {
   it("should return the correct key condition expression", () => {
     const result = getKeyConditionExpression({ name: "test" });
-    expect(result).toBe("name = :name");
+    expect(result).toBe("#name = :name");
   });
 });
 
@@ -11,5 +11,12 @@ describe("Given the getExpressionAttributeValues function", () => {
   it("should return the correct expression attribute values", () => {
     const result = getExpressionAttributeValues({ name: "test" });
     expect(result).toEqual({ ":name": "test" });
+  });
+});
+
+describe("Given the getExpressionAttributeNames function", () => {
+  it("should return the correct expression attribute names", () => {
+    const result = getExpressionAttributeNames({ name: "test" });
+    expect(result).toEqual({ "#name": "name" });
   });
 });
