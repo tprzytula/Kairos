5fb22abcc737a69da94dc45f5c38df8a4646b92c
tomasz.przytula <t.przytula.dev@gmail.com>
2025-05-01 16:00:53 +0100

    merge dynamodb databases for defaultS

--- packages/infra/modules/dynamodb/outputs.tf
+++ packages/infra/modules/dynamodb/outputs.tf
@@ -2,14 +2,10 @@ output "grocery_list_arn" {
   value = aws_dynamodb_table.grocery_list.arn
 }
 
-output "grocery_items_icons_arn" {
-  value = aws_dynamodb_table.grocery_items_icons.arn
-}
-
-output "grocery_items_default_units_arn" {
-  value = aws_dynamodb_table.grocery_items_default_units.arn
-}
-
 output "noise_tracking_arn" {
   value = aws_dynamodb_table.noise_tracking.arn
+}
+
+output "grocery_items_defaults_arn" {
+  value = aws_dynamodb_table.grocery_items_defaults.arn
 }
