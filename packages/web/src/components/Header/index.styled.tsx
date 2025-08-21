f31d08e5c17ca0ac30897aab9bb4f1aa65889d03
tomasz.przytula <t.przytula.dev@gmail.com>
2025-08-21 15:17:38 +0100

    fix headers appearance

--- packages/web/src/components/Header/index.styled.tsx
+++ packages/web/src/components/Header/index.styled.tsx
@@ -8,22 +8,12 @@ export const StyledHeader = styled(Typography)(({ theme }) => ({
   fontWeight: 'bold',
   color: theme.palette.primary.main,
   textShadow: '1px 1px 0px rgba(0,0,0,0.1)',
-  borderBottom: `3px solid ${theme.palette.error.main}`,
+
   position: 'relative',
   margin: '0 0 0.5em 0',
   transition: 'all 200ms ease-in-out',
   '&:hover': {
     color: theme.palette.custom?.hover?.primary,
     transform: 'translateY(-1px)',
-  },
-  '&::after': {
-    content: '""',
-    position: 'absolute',
-    bottom: -4,
-    left: '30%',
-    width: '40%',
-    height: '4px',
-    background: theme.palette.primary.main,
-    transition: 'all 200ms ease-in-out',
   },
 }))
