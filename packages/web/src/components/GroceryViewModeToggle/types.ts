e8aed294c99f45fa4ec558b1a7e189027836c2c6
tomasz.przytula <t.przytula.dev@gmail.com>
2025-08-23 14:00:55 +0100

    clean: remove obsolete action button components
    Remove unused components now replaced by ActionButtonsBar:
    - MarkToDoItemsAsDoneButton (3 files: component, test, styles)
    - RemovePurchasedItemsButton (3 files: component, test, styles)
    - GroceryViewModeToggle (2 files: component, types)
    
    All functionality has been consolidated into the reusable ActionButtonsBar component.
    This reduces codebase size and eliminates duplicate/obsolete code.

--- packages/web/src/components/GroceryViewModeToggle/types.ts
+++
@@ -1,6 +0,0 @@
-import { GroceryViewMode } from '../../enums/groceryCategory'
-
-export interface IGroceryViewModeToggleProps {
-  viewMode: GroceryViewMode
-  onViewModeChange: (mode: GroceryViewMode) => void
-}
