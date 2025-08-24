7e0b7dc34a423ba4ee5a9c6517b86c8614a24319
tomasz.przytula <t.przytula.dev@gmail.com>
2025-08-24 17:15:25 +0100

    fix: correct PushSubscriptions table name and add missing GSI index
    - Fixed table name mismatch: was push_subscriptions_{random} now PushSubscriptions
    - Added UserPushSubscriptionsIndex GSI for userId queries
    - Resolves AccessDeniedException when saving push subscriptions

--- packages/infra/modules/dynamodb/main.push_subscriptions.tf
+++ packages/infra/modules/dynamodb/main.push_subscriptions.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "push_subscriptions" {
-  name           = format("push_subscriptions_%s", var.random_name)
+  name           = "PushSubscriptions"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "userId"
   range_key      = "endpoint"
@@ -12,6 +12,12 @@ resource "aws_dynamodb_table" "push_subscriptions" {
   attribute {
     name = "endpoint"
     type = "S"
+  }
+
+  global_secondary_index {
+    name            = "UserPushSubscriptionsIndex"
+    hash_key        = "userId"
+    projection_type = "ALL"
   }
 
   tags = {
